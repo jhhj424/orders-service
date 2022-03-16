6d7a83a5976202fbb3042e5d4d77182fefd141de
zito-s <jhhj424@naver.com>
2022-03-16 18:19:39 +0900

    feat: add OrderRepository query method

--- src/main/java/com/example/ordersservice/entity/OrderRepository.java
+++ src/main/java/com/example/ordersservice/entity/OrderRepository.java
@@ -3,4 +3,7 @@ package com.example.ordersservice.entity;
 import org.springframework.data.repository.CrudRepository;
 
 public interface OrderRepository extends CrudRepository<Order, Long> {
+    Order findByOrderId(String orderId);
+
+    Iterable<Order> findByUserId(String userId);
 }
